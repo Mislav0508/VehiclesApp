84a07b3aa81c9eb329d5441b7f80868f804d3b2a
Mislav0508 <mislav0508@hotmail.com>
2021-01-21 06:15:31 +0100

    small update

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import Button from 'react-bootstrap/Button'
+import {useObserver} from "mobx-react"
 
 export const VehicleCard = ({id,make,model,year}) => {
 
-  return (
-    <div className="grid-item" key={id}>
-      <h3>{make}</h3>      
-      <h4>Model: {model}</h4>
-      <h4>Year: {year}</h4>
-      <div className="details-div">
-        <Link to={`/${id}`}>
-          <Button>Details</Button>
-        </Link>
+  return useObserver(()=> (
+    (
+      <div className="grid-item" key={id}>
+        <h3>{make}</h3>      
+        <h4>Model: {model}</h4>
+        <h4>Year: {year}</h4>
+        <div className="details-div">
+          <Link to={`/${id}`}>
+            <Button>Details</Button>
+          </Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  ))
 }
